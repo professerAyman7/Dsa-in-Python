75eec4f093dc424e7f9ceea225d9ba9bd8daa545
Ayman <professerayman@gmail.com>
2026-09-25 16:23:50 +0530

    Updated

--- 0. LeetCode Question/7. LeetCode 121/1. LeetCode 121.py
+++ 0. LeetCode Question/7. LeetCode 121/1. LeetCode 121.py
@@ -7,6 +7,7 @@ def bestTime(prices):
         if price < minPrice:
             minPrice = price
 
+        # We dont need to check if we can update it directly
         if price > minPrice:
             profit = price - minPrice
             
@@ -17,3 +18,9 @@ def bestTime(prices):
 
 prices = [7,1,5,3,6,4]
 print(bestTime(prices))
+
+
+'''
+Time  Complexity - O(n)
+Space Complexity - O(1)
+'''
